fb6d273faaa8a3da46e065c9cb460cf4690acfe2
Vitaliy <theyamshikov@yandex.ru>
2020-12-31 20:13:03 +0600

    upd

--- src/Domain/Encoders/RequestEncoder.php
+++ src/Domain/Encoders/RequestEncoder.php
@@ -7,6 +7,15 @@ use ZnLib\Rpc\Domain\Interfaces\Encoders\RequestEncoderInterface;
 class RequestEncoder implements RequestEncoderInterface
 {
 
+    private $bodyName;
+    private $metaName;
+
+    public function __construct(string $bodyName = 'body', string $metaName = 'meta')
+    {
+        $this->bodyName = $bodyName;
+        $this->metaName = $metaName;
+    }
+
     public function encode($data)
     {
         $request = [];
@@ -17,10 +26,10 @@ class RequestEncoder implements RequestEncoderInterface
             $request['method'] = $data['method'];
         }
         if(isset($data['params'])) {
-            $request['params']['body'] = $data['params'];
+            $request['params'][$this->bodyName] = $data['params'];
         }
         if(isset($data['meta'])) {
-            $request['params']['meta'] = $data['meta'];
+            $request['params'][$this->metaName] = $data['meta'];
         }
         if (isset($data['id'])) {
             $request['id'] = $data['id'];
@@ -37,11 +46,11 @@ class RequestEncoder implements RequestEncoderInterface
         if (isset($data['method'])) {
             $request['method'] = $data['method'];
         }
-        if(isset($data['params']['body'])) {
-            $request['params'] = $data['params']['body'];
+        if(isset($data['params'][$this->bodyName])) {
+            $request['params'] = $data['params'][$this->bodyName];
         }
-        if(isset($data['params']['meta'])) {
-            $request['meta'] = $data['params']['meta'];
+        if(isset($data['params'][$this->metaName])) {
+            $request['meta'] = $data['params'][$this->metaName];
         }
         if (isset($data['id'])) {
             $request['id'] = $data['id'];
--- src/Domain/Encoders/ResponseEncoder.php
+++ src/Domain/Encoders/ResponseEncoder.php
@@ -7,6 +7,15 @@ use ZnLib\Rpc\Domain\Interfaces\Encoders\ResponseEncoderInterface;
 class ResponseEncoder implements ResponseEncoderInterface
 {
 
+    private $bodyName;
+    private $metaName;
+
+    public function __construct(string $bodyName = 'body', string $metaName = 'meta')
+    {
+        $this->bodyName = $bodyName;
+        $this->metaName = $metaName;
+    }
+
     public function encode($data)
     {
         $response = [];
@@ -14,10 +23,10 @@ class ResponseEncoder implements ResponseEncoderInterface
             $response['jsonrpc'] = $data['jsonrpc'];
         }
         if (isset($data['result'])) {
-            $response['result']['body'] = $data['result'];
+            $response['result'][$this->bodyName] = $data['result'];
         }
         if (isset($data['meta'])) {
-            $response['result']['meta'] = $data['meta'];
+            $response['result'][$this->metaName] = $data['meta'];
         }
         if (isset($data['error'])) {
             $response['error'] = $data['error'];
@@ -34,11 +43,11 @@ class ResponseEncoder implements ResponseEncoderInterface
         if (isset($data['jsonrpc'])) {
             $response['jsonrpc'] = $data['jsonrpc'];
         }
-        if (isset($data['result']['body'])) {
-            $response['result'] = $data['result']['body'];
+        if (isset($data['result'][$this->bodyName])) {
+            $response['result'] = $data['result'][$this->bodyName];
         }
-        if (isset($data['result']['meta'])) {
-            $response['meta'] = $data['result']['meta'];
+        if (isset($data['result'][$this->metaName])) {
+            $response['meta'] = $data['result'][$this->metaName];
         }
         if (isset($data['error'])) {
             $response['error'] = $data['error'];
