385031afb2d88e4b07dae06e19e8c0b5c1a7dc6a
Vitaliy <theyamshikov@yandex.ru>
2021-01-13 10:45:12 +0600

    upd

---
+++ src/Symfony4/Web/config/routing.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use ZnLib\Rpc\Symfony4\Web\Controllers\DefaultController;
+use ZnLib\Rpc\Symfony4\Web\Controllers\DocsController;
+use ZnLib\Rpc\Symfony4\Web\Controllers\RpcController;
+
+return function (RoutingConfigurator $routes) {
+    $routes
+        ->add('main_page', '/')
+        ->controller([DefaultController::class, 'index']);
+
+    $routes
+        ->add('docs', '/json-rpc')
+        ->controller([DocsController::class, 'showDocs'])
+        ->methods(['GET']);
+
+    $routes
+        ->add('call_procedure', '/json-rpc')
+        ->controller([RpcController::class, 'callProcedure'])
+        ->methods(['POST']);
+};
