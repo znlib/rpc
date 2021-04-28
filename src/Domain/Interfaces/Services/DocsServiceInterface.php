c85f9a5690ee89a348861417641fe00174726173
Vitaliy <theyamshikov@yandex.ru>
2021-04-28 11:55:08 +0600

    upd

--- src/Domain/Interfaces/Services/DocsServiceInterface.php
+++ src/Domain/Interfaces/Services/DocsServiceInterface.php
@@ -2,8 +2,11 @@
 
 namespace ZnLib\Rpc\Domain\Interfaces\Services;
 
+use ZnLib\Rpc\Domain\Entities\DocEntity;
+
 interface DocsServiceInterface
 {
 
+    public function oneByName(string $name): DocEntity;
     public function loadByName(string $name): string;
 }
