b55aad617ba03f44b531c767e93d8664ae1e50d5
Vitaliy <theyamshikov@yandex.ru>
2021-11-10 15:43:29 +0600

    upd

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,19 @@
 <?php
 
-\ZnCore\Base\Libs\DotEnv\DotEnv::init();
+//\ZnCore\Base\Libs\DotEnv\DotEnv::init();
+
+
+$container = \Illuminate\Container\Container::getInstance();
+$znCore = new \ZnSandbox\Sandbox\App\Libs\ZnCore($container);
+$znCore->init();
+
+/** @var \ZnSandbox\Sandbox\App\Interfaces\AppInterface $appFactory */
+$appFactory = $container->get(\ZnTool\Test\Libs\TestApp::class);
+$appFactory->setBundles([
+    
+]);
+$appFactory->init();
+
+/*$znCore->loadBundles([
+    
+]);*/
