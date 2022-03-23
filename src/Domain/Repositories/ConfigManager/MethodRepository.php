bd846182a7069c4f7822ed92d061939fd4e227ba
ZnDeveloper <zndev@ya.ru>
2022-03-23 19:25:41 +0600

    upd

--- src/Domain/Repositories/ConfigManager/MethodRepository.php
+++ src/Domain/Repositories/ConfigManager/MethodRepository.php
@@ -2,6 +2,7 @@
 
 namespace ZnLib\Rpc\Domain\Repositories\ConfigManager;
 
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use ZnCore\Base\Libs\Store\StoreFile;
 use ZnCore\Domain\Base\Repositories\BaseFileCrudRepository;
@@ -15,11 +16,14 @@ use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
 class MethodRepository extends BaseFileCrudRepository implements MethodRepositoryInterface
 {
 
-    private $items;
+    const CACHE_KEY = 'rpcMethodCollection';
+    
+    private $cache;
 
-    public function __construct(EntityManagerInterface $em, CacheInterface $cache)
+    public function __construct(EntityManagerInterface $em, AdapterInterface $cache)
     {
         parent::__construct($em);
+        $this->cache = $cache;
     }
 
     public function fileName(): string
@@ -42,6 +46,13 @@ class MethodRepository extends BaseFileCrudRepository implements MethodRepositor
 
     protected function getItems(): array
     {
-        return \ZnLib\Rpc\Domain\Helpers\RoutesHelper::getAllRoutes();
+        $cacheItem = $this->cache->getItem(self::CACHE_KEY);
+        if($cacheItem->get() == null) {
+            $collection = \ZnLib\Rpc\Domain\Helpers\RoutesHelper::getAllRoutes();
+            $cacheItem->set($collection);
+            $cacheItem->expiresAfter(60);
+            $this->cache->save($cacheItem);
+        }
+        return $cacheItem->get();
     }
 }
