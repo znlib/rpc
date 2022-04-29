5c8a9605df1aee93c06c4e7e9e2cf42b2fa0a28b
Vitaliy <theyamshikov@yandex.ru>
2022-04-29 19:14:30 +0600

    upd

--- src/Domain/Subscribers/LanguageSubscriber.php
+++ src/Domain/Subscribers/LanguageSubscriber.php
@@ -2,17 +2,12 @@
 
 namespace ZnLib\Rpc\Domain\Subscribers;
 
-use ZnBundle\Language\Domain\Interfaces\Services\RuntimeLanguageServiceInterface;
-use ZnSandbox\Sandbox\Rpc\Domain\Enums\RpcCryptoProviderStrategyEnum;
-use ZnSandbox\Sandbox\Rpc\Domain\Enums\RpcEventEnum;
-use ZnSandbox\Sandbox\Rpc\Domain\Events\RpcRequestEvent;
-use ZnSandbox\Sandbox\Rpc\Domain\Events\RpcResponseEvent;
-use ZnSandbox\Sandbox\Rpc\Domain\Interfaces\Services\SettingsServiceInterface;
-use ZnSandbox\Sandbox\Rpc\Symfony4\Web\Libs\CryptoProviderInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use ZnBundle\Language\Domain\Interfaces\Services\RuntimeLanguageServiceInterface;
 use ZnCore\Domain\Traits\EntityManagerTrait;
 use ZnLib\Rpc\Domain\Enums\HttpHeaderEnum;
-use ZnLib\Rpc\Domain\Exceptions\InvalidRequestException;
+use ZnSandbox\Sandbox\Rpc\Domain\Enums\RpcEventEnum;
+use ZnSandbox\Sandbox\Rpc\Domain\Events\RpcRequestEvent;
 
 class LanguageSubscriber implements EventSubscriberInterface
 {
@@ -36,7 +31,7 @@ class LanguageSubscriber implements EventSubscriberInterface
     public function onBeforeRunAction(RpcRequestEvent $event)
     {
         $languageCode = $event->getRequestEntity()->getMetaItem(HttpHeaderEnum::LANGUAGE);
-        if(!empty($languageCode)) {
+        if (!empty($languageCode)) {
             $this->languageService->setLanguage($languageCode);
         }
     }
