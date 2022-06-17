7fca51d8b29a6e4b2722a06731239e0f8b4e7787
Vitaliy <theyamshikov@yandex.ru>
2022-06-17 20:22:08 +0600

    upd

---
+++ src/Domain/Libs/BundleLoaders/SymfonyRpcRoutesLoader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ZnLib\Rpc\Domain\Libs\BundleLoaders;
+
+use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
+use ZnCore\Base\Libs\App\Interfaces\ConfigManagerInterface;
+use ZnCore\Base\Libs\App\Loaders\BundleLoaders\BaseLoader;
+
+class SymfonyRpcRoutesLoader extends BaseLoader
+{
+
+    /*public function __construct(ConfigManagerInterface $configManager)
+    {
+        $this->setConfigManager($configManager);
+    }*/
+
+    public function loadAll(array $bundles): array
+    {
+        $config = [];
+        foreach ($bundles as $bundle) {
+            $loadedConfig = $this->load($bundle);
+            $config = ArrayHelper::merge($config, $loadedConfig);
+        }
+//        $_ENV['RPC_ROUTES'] = $config;
+        $this->getConfigManager()->set('rpcRoutes', $config);
+        return [];
+//        return $config ? ['rpcRoutes' => $config] : [];
+    }
+}
