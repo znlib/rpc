0b390b557d569462168641ff3ba13f02e25757ea
ZnDeveloper <zndev@ya.ru>
2022-06-18 21:19:29 +0600

    upd

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,10 @@
 
 
 $container = new \Illuminate\Container\Container();
-$znCore = new \ZnSandbox\Sandbox\App\Libs\ZnCore($container);
+$znCore = new \ZnCore\Base\Libs\App\Libs\ZnCore($container);
 $znCore->init();
 
-/** @var \ZnSandbox\Sandbox\App\Interfaces\AppInterface $appFactory */
+/** @var \ZnCore\Base\Libs\App\Interfaces\AppInterface $appFactory */
 $appFactory = $container->get(\ZnTool\Test\Libs\TestApp::class);
 $appFactory->setBundles([
     
