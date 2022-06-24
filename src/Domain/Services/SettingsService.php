a56c6ba6ac7db84affdaf8456b7a25b78e5b014a
ZnDeveloper <zndev@ya.ru>
2022-06-25 01:12:46 +0600

    upd

--- src/Domain/Services/SettingsService.php
+++ src/Domain/Services/SettingsService.php
@@ -2,11 +2,11 @@
 
 namespace ZnLib\Rpc\Domain\Services;
 
+use ZnCore\Base\Validation\Helpers\ValidationHelper;
+use ZnCore\Domain\Entity\Helpers\EntityHelper;
 use ZnLib\Rpc\Domain\Entities\SettingsEntity;
 use ZnLib\Rpc\Domain\Interfaces\Services\SettingsServiceInterface;
 use ZnSandbox\Sandbox\Settings\Domain\Interfaces\Services\SystemServiceInterface;
-use ZnCore\Domain\Entity\Helpers\EntityHelper;
-use ZnCore\Base\Validation\Helpers\ValidationHelper;
 
 class SettingsService implements SettingsServiceInterface
 {
@@ -34,7 +34,6 @@ class SettingsService implements SettingsServiceInterface
     {
         $data = $this->systemService->view('rpc');
         $settingsEntity = new SettingsEntity();
-//        exit(json_encode($data, JSON_PRETTY_PRINT));
         EntityHelper::setAttributes($settingsEntity, $data);
         return $settingsEntity;
     }
